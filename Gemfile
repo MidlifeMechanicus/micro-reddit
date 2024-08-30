f45b94df26fdcf35837f11f964e12f4d9a6bafda
ElectronDragon <electricdragondevelopment@gmail.com>
2024-08-30 08:13:57 +0100

    Correct improper validation syntax for Post :title and :body length

--- Gemfile
+++ Gemfile
@@ -55,3 +55,5 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
 end
+
+gem "rubocop", "~> 1.65"
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
-  validates :title, presence: true, length { minimum: 3}
-  validates :body, presence: true, length { minumim: 3 }
+  validates :title, presence: true, length: { minimum: 3 }
+  validates :body, presence: true, length: { minimum: 3 }
   validates :user_id, presence: true
   belongs_to :user
 end
