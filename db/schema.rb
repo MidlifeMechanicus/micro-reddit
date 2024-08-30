11f720f5cb27cfca92036effbd3b92e91dc97e31
ElectronDragon <electricdragondevelopment@gmail.com>
2024-08-30 07:52:46 +0100

    Generate Post model with title, body and user_id columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_30_061332) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_30_064941) do
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
@@ -18,4 +27,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_08_30_061332) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "posts", "users"
 end
