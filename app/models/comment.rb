7ea0437c1a0a9cdcdf84d33907bf14235d5029df
ElectronDragon <electricdragondevelopment@gmail.com>
2024-08-31 16:12:18 +0100

    Migrate db; add comment validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ApplicationRecord
+  validates :body, presence: true, length: { minimum: 3 }
   belongs_to :user
   belongs_to :post
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_30_064941) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_31_150923) do
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -28,5 +38,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_08_30_064941) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
